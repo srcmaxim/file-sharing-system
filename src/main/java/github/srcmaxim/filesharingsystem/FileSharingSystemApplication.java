eb327d320174699193f7125cd1150cdcd0022a87
srcmaxim <srcmaxim@gmail.com>
2017-08-01 05:12:51 +0300

    #1 Generate project

---
+++ src/main/java/github/srcmaxim/filesharingsystem/FileSharingSystemApplication.java
@@ -0,0 +1,12 @@
+package github.srcmaxim.filesharingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSharingSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/github/srcmaxim/filesharingsystem/FileSharingSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package github.srcmaxim.filesharingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileSharingSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

fa65466545a3c8bf99f3a5651922fd0f1e37b493
srcmaxim <srcmaxim@gmail.com>
2017-10-03 22:14:07 +0300

    #61 Create info and error pages for showing information

---
+++ src/main/java/github/srcmaxim/filesharingsystem/controller/AwareController.java
@@ -0,0 +1,27 @@
+package github.srcmaxim.filesharingsystem.controller;
+
+import github.srcmaxim.filesharingsystem.annotation.Loggable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@Loggable
+public class AwareController {
+
+    @RequestMapping(value = "/info", params = "type")
+    public String info(@RequestParam String type, Model model) {
+        model.addAttribute("group", "info");
+        model.addAttribute("type", type);
+        return "aware";
+    }
+
+    @RequestMapping(value = "/error", params = "type")
+    public String error(@RequestParam String type, Model model) {
+        model.addAttribute("group", "error");
+        model.addAttribute("type", type);
+        return "aware";
+    }
+
+}

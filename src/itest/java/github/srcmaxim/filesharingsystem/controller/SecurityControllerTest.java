d1bcb1d0d41ffb96a6756642da715fa44af5ee4b
srcmaxim <srcmaxim@gmail.com>
2017-10-28 23:08:50 +0300

    #62 Test registration when registration is ok

---
+++ src/itest/java/github/srcmaxim/filesharingsystem/controller/SecurityControllerTest.java
@@ -0,0 +1,104 @@
+package github.srcmaxim.filesharingsystem.controller;
+
+import github.srcmaxim.filesharingsystem.model.Authority;
+import github.srcmaxim.filesharingsystem.model.User;
+import github.srcmaxim.filesharingsystem.model.VerificationToken;
+import github.srcmaxim.filesharingsystem.repository.UserRepository;
+import github.srcmaxim.filesharingsystem.repository.VerificationTokenRepository;
+import github.srcmaxim.filesharingsystem.service.EmailService;
+import github.srcmaxim.filesharingsystem.service.UserService;
+import github.srcmaxim.filesharingsystem.system.DbConfig;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+@Import(DbConfig.class)
+public class SecurityControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    private User user;
+
+    private static MockHttpSession session;
+
+    @MockBean
+    private EmailService emailService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private VerificationTokenRepository tokenRepository;
+
+    @BeforeClass
+    public static void setupSession() {
+        session = new CustomHttpSession("user1", "12345qaz", asList(Authority.values()));
+    }
+
+    @Before
+    public void setup() {
+        user = User.createNewUser("user1", "12345qaz");
+        user.setId(1L);
+        user.setFirstName("UserOne");
+        user.setLastName("UserOne");
+        user.setPhone("+1(111)-111-1111");
+        user.setEmail("user1@gmail.com");
+    }
+
+    @Test
+    public void shouldEmailVerificationTokenToUserAndSetUserNotVerifiedWhenRegistrationSuccess() throws Exception {
+        doNothing().when(userService).principalsExist(any(), any());
+        when(userService.createUserAccount(any(), any())).thenAnswer(invocation -> {
+            try {
+                invocation.callRealMethod();
+            } catch (Throwable throwable) {
+                // do nothing
+            }
+            return user;
+        });
+        doAnswer(invocation -> invocation.getArguments()[0]).when(userRepository).save((User) any());
+        doAnswer(invocation -> invocation.getArguments()[0]).when(tokenRepository).save((VerificationToken) any());
+
+
+        mvc.perform(post("/register").session(session).with(csrf())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", user.getId().toString())
+                .param("login", user.getLogin())
+                .param("password", user.getPassword())
+                .param("passwordConfirmation", user.getPassword())
+                .param("firstName", user.getFirstName())
+                .param("lastName", user.getLastName())
+                .param("email", user.getEmail())
+                .param("phone", user.getPhone())
+        )
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrlPattern("/info?type=*"));
+
+        assertFalse(user.isEnabled());
+    }
+
+}
